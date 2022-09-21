66f12bd8bd9f38675f72cf7a023100acf73decc4
Onyiana <adaurecananaba@gmail.com>
2022-09-21 23:19:22 +0100

    updated

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+const menuOpen = document.querySelector(".ri-menu-add-line")
+const menuClose = document.querySelector(".ri-close-circle-fill")
+const navMenu = document.querySelector(".navlinks")
+
+menuOpen.addEventListener("click", function(){
+    navMenu.style.right = '0px'
+    menuOpen.style.display = 'none'
+    menuClose.style.display = 'block'
+    // navBar.style.right = "0px"
+})
+
+menuClose.addEventListener("click", function(){
+    navMenu.style.right = "-300px"
+    menuOpen.style.display = 'block'
+    menuClose.style.display = 'none'
+})
+
+const navLists = document.querySelectorAll('li')
+navLists.forEach((element) => {
+    element.addEventListener('click', function(){
+        navMenu.style.right = '-300px'
+        menuClose.style.display = 'none'
+        menuOpen.style.display = 'block'
+    })
+})
